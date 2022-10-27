90a487b6766b848000ffba9b5fb0ecee42a86423
NgoTruongKhai <83157358+NgoTruongKhai@users.noreply.github.com>
2022-10-27 21:14:20 +0700

    Update CategoryFactory.php

--- backend/database/factories/CategoryFactory.php
+++ backend/database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'description', $this->faker->text(50)
+            'description'=> $this->faker->text(50)
         ];
     }
 }
